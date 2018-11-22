75f1fe0d775e43790496bec0e321921af1d5db4e
Brian Vanegas <brianvanegas@viajesaeromar.com>
2018-11-22 22:15:31 +0000

    menu

--- src/hileras/Hilera.java
+++ src/hileras/Hilera.java
@@ -14,18 +14,35 @@ public class Hilera {
     public static void main(String[] args)
     {
         String texto;
-        char aCar;
+        char aCar, opc;
         int i = 0;
 
         ListaHileraDoble hilera = new ListaHileraDoble();
+        do{
+            opc = Menu();
+            switch (opc) {
+                case '1':
+                    texto = JOptionPane.showInputDialog(null, "Ingrese texto para crear la hilera", "initial value");
+                    break;
+                case '2':
+                    /*while (i < texto.length()) {
+                        System.out.println(texto.charAt(i));
+                        hilera.CrearHilera(texto.charAt(i));
+                        i++;
+                    }*/
+                    break;
+            }
+        } while(opc != '5');
+    }
 
-        texto = JOptionPane.showInputDialog(null, "Ingrese texto para crear la hilera", "Crear Hilera", JOptionPane.INFORMATION_MESSAGE);
+    static char Menu()
+    {
+        char opcion;
+        do{
+            opcion = JOptionPane.showInputDialog(null, "\n1. Crear o editar hilera\n2. Mostrar hilera\n3. Insertar\n4. Eliminar\n5. Salir").charAt(0);
+        } while(opcion < '1' || opcion > '5');
 
-        while (i < texto.length()) {
-            System.out.println(texto.charAt(i));
-            hilera.CrearHilera(texto.charAt(i));
-            i++;
-        }
+        return opcion;
     }
 
 }
